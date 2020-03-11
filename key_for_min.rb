51f51e2037ca6fc2508dec194cea726ba83ec6d8
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-11 22:49:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,6 +6,11 @@ def key_for_min_value(name_hash)
   smallest_key = nil 
   smallest_value = nil 
   if smallest_value == nil || value < smallest_value
-    
+    smallest_value = value
+      smallest_key = key
+    end
+  end
+  smallest_key
+end
 
 end
