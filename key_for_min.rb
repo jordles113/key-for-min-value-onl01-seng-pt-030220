15a8407f0091353db3d837f11866160997632070
Jordan Romero <jordan.t.romero@gmail.com>
2020-03-11 22:50:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.map |key, value| 
-  smallest_key = nil 
-  smallest_value = nil 
-  if smallest_value == nil || value < smallest_value
-    smallest_value = value
-      smallest_key = key
-    end
-  end
-  smallest_key
+  
 end
 
